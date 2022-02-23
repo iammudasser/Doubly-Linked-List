3481467a650859abcf89aaaf084edb481ad076b3
iammudasser <82773366+iammudasser@users.noreply.github.com>
2022-02-23 21:25:23 +0500

    Add files via upload

---
+++ DList.java
@@ -0,0 +1,68 @@
+package DataStructure;
+
+public class DList {
+
+	private DListNode head;
+	private DListNode tail;
+
+	class DListNode {
+
+		int data;
+		DListNode next;
+		DListNode prev;
+
+		public DListNode(int data) {
+
+			this.data = data;
+			this.next = null;
+			this.prev = null;
+
+		}
+	}
+
+	public void insertNodeHead(int data) {
+		DListNode newNode = new DListNode(data);
+
+		if (head == null) {
+
+			head = newNode;
+			tail = newNode;
+
+		}
+
+		else {
+
+			newNode.next = head;
+			head.prev = newNode;
+			head = newNode;
+
+		}
+
+	}
+
+	public void delNodeTail() {
+		tail.prev.next = null;
+		tail = tail.prev;
+
+	}
+
+	public void delNodeFront() {
+		head.next.prev = null;
+		head = head.next;
+
+	}
+
+	public void print() {
+
+		DListNode currNode = head;
+
+		while (currNode != null) {
+			System.out.print(currNode.data + " " + " ");
+
+			currNode = currNode.next;
+		}
+		System.out.println("NULL");
+
+	}
+
+}
